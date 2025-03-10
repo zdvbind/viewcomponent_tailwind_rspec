4366394410637ffe3b03b95fbe854dbbdf59af7d
Dmitry Zaguta <zdvbind@gmail.com>
2025-03-10 18:56:20 +0500

    add form errors component

---
+++ app/components/form_errors_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class FormErrorsComponent < ApplicationComponent
+  def initialize(resource:, descriptor:)
+    @resource = resource
+    @descriptor = descriptor
+  end
+
+end
---
+++ spec/components/form_errors_component_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe FormErrorsComponent, type: :component do
+  let(:team) { FactoryBot.create(:team) }
+  let(:player) { FactoryBot.create(:athlete, team: team) }
+
+  it "renders a player's error list" do
+    render_inline(described_class.new(resource: player, descriptor: "player"))
+
+    expect(rendered_content).to(
+      have_content("0 errors")
+        .and have_content("prevented this player from being saved")
+    )
+  end
+end
